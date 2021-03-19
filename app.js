15ff9a693b301614ff2b6d82c7142043cd323110
Shweta Kumari <201552042@iiitvadodara.ac.in>
2021-03-19 21:43:28 +0530

    testing

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/', async (req, res, next) => {
     const urlExists = await ShortUrl.findOne({ url })
     if (urlExists) {
       res.render('index', {
-        short_url: `${req.headers.host}/${urlExists.shortId}`,
+        short_url: `/${urlExists.shortId}`,
       })
       return
     }
